529d7c5d9d5d4c49e65b1436ebec2b56faf9690e
S.P. Vishwakarma <itspratap255@gmail.com>
2026-08-08 14:24:22 +0530

    Backend Architecture design

---
+++ crash_handle/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class CrashHandleConfig(AppConfig):
+    name = 'crash_handle'
---
+++ data_proccess/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class DataProccessConfig(AppConfig):
+    name = 'data_proccess'
