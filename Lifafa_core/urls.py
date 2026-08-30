ab5470e8c42d52acae20114f378bde386193e9a0
S.P. Vishwakarma <itspratap255@gmail.com>
2026-08-30 12:57:52 +0530

    Enhance pages

--- Lifafa_core/urls.py
+++ Lifafa_core/urls.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 from django.urls import path , include
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
     path('account/', include('account.urls')),
-    # path('dashboard/', include('data_process.urls')),
+    path('special/', include('data_proccess.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- data_proccess/views.py
+++ data_proccess/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+def enjoy(request):
+    return render(request, 'customization/for-you.html')
+
+def celeb(request):
+    return render(request, 'customization/only-for-you.html')
